8d263af75496ec8086e7b80b7a5dd06bb58a145b
Maxime Bordet <maxime.bordet83@gmail.com>
2024-02-16 11:19:09 +0100

    deploy

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import logo from '../../Graph/logo_web&wave.png';
 import './contact.css';
 
@@ -9,7 +9,7 @@ const Contact = () => {
       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100vh' }}>
         <img src={logo} alt="" />
 
-      <iframe style={{width:'100%'}} src="https://docs.google.com/forms/d/e/1FAIpQLSfMYZSl7ypqryDdrMYf09Ny-ZZd5V_PgpCEWxoIvZJTVDL_eg/viewform?embedded=true" width="640" height="959" frameborder="0" marginheight="0" marginwidth="0">Chargement…</iframe>
+      <iframe title="Formulaire de contact" style={{width:'100%'}} src="https://docs.google.com/forms/d/e/1FAIpQLSfMYZSl7ypqryDdrMYf09Ny-ZZd5V_PgpCEWxoIvZJTVDL_eg/viewform?embedded=true" width="640" height="959" frameborder="0" marginheight="0" marginwidth="0">Chargement…</iframe>
     </div>
     </div>
   );
